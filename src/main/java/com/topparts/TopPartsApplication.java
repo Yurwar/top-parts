9c94e1170754e5ad1f08fa9b54b8d8594cddfcc3
Yurii Matora <yura.matora11@gmail.com>
2019-10-01 23:16:37 +0300

    Create project structure
    Create Spring project structure for top-parts application using Spring Initializr

---
+++ src/main/java/com/topparts/TopPartsApplication.java
@@ -0,0 +1,11 @@
+package com.topparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopPartsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TopPartsApplication.class, args);
+    }
+}
