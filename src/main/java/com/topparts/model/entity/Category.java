70ac22e6ba73a53f1a69492fa58ab3c73a7dfbe3
CodeGangsta44 <dovorman44@gmail.com>
2019-10-14 19:32:14 +0300

    Fixed method naming, added field and lombok annotations to Category

--- src/main/java/com/topparts/model/entity/Category.java
+++ src/main/java/com/topparts/model/entity/Category.java
@@ -1,10 +1,19 @@
 package com.topparts.model.entity;
 
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
 
 @Entity
 @Table(name = "categories")
@@ -15,6 +24,9 @@ public class Category {
     @Column(name = "category_id", nullable = false)
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id", insertable = false, updatable = false)
     private Category parentCategory;
@@ -22,15 +34,11 @@ public class Category {
     @ElementCollection(targetClass = String.class)
     private Set<String> properties;
 
-    public Category(String ... properties) {
-        this.properties = new HashSet<String>(Arrays.asList(properties));
-    }
-
-    public Set<String> getProperties() {
+    public Set<String> getAllProperties() {
         Set<String> result = new HashSet<>(properties);
 
         if (!Objects.isNull(parentCategory)) {
-            result.addAll(parentCategory.getProperties());
+            result.addAll(parentCategory.getAllProperties());
         }
 
         return result;
