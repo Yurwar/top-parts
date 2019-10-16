5910a99128f1916faa6a72abb773d7cbb891d611
Yurii Matora <yura.matora11@gmail.com>
2019-10-16 19:46:19 +0300

    feat: add frame to suppliers services
    Add package structure and stubs for suppliers services
    in top-parts app
    
    Signed-off-by: Yurii Matora <yura.matora11@gmail.com>

---
+++ top-parts-main/src/main/java/com/topparts/model/service/suppliers/price/PriceSupplierService.java
@@ -0,0 +1,41 @@
+package com.topparts.model.service.suppliers.price;
+
+import com.topparts.model.entity.Product;
+import com.topparts.model.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PriceSupplierService implements ProductService {
+    @Override
+    public void createProduct(Product product) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Product> getAllProductsByCategory(Long id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void updateProduct(Long id, Product product) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        throw new UnsupportedOperationException();
+    }
+}
