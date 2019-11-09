59dcc7737b97698c8fe45bb041d38145dd8682b6
Yurii Matora <yura.matora11@gmail.com>
2019-11-09 18:02:10 +0200

    Add delay to the search supplier service methods
    Delay response of service methods by using thread sleep method
    and random generating time of delay.

--- search-supplier/src/main/java/com/topparts/searchsupplier/model/service/impl/ProductServiceImpl.java
+++ search-supplier/src/main/java/com/topparts/searchsupplier/model/service/impl/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import com.topparts.searchsupplier.model.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -17,11 +18,27 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
+        delayResponseInSeconds(15, 20);
         return productRepository.findAll();
     }
 
     @Override
     public List<Product> getAllProductsByQuery(String query) {
+        delayResponseInSeconds(15, 20);
         return productRepository.findAllByNameContainingOrDescriptionContaining(query, query);
+    }
+
+    private void delayResponseInSeconds(int minDelay, int maxDelay) {
+        try {
+            Thread.sleep(generateResponseDelayInSeconds(minDelay, maxDelay) * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int generateResponseDelayInSeconds(int min, int max) {
+        Random random = new Random();
+        ++max;
+        return random.nextInt(max - min) + min;
     }
 }
