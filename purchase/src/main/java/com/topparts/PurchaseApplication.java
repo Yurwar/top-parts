86df3f177abec889eb927933b50bebbdac2a8bc4
Yurii Matora <yura.matora11@gmail.com>
2019-12-04 01:39:01 +0200

    feat: implement simple services for modules

--- purchase/src/main/java/com/topparts/PurchaseApplication.java
+++ purchase/src/main/java/com/topparts/PurchaseApplication.java
@@ -3,13 +3,11 @@ package com.topparts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableDiscoveryClient
-public class DiscoveryApplication {
+public class PurchaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryApplication.class, args);
+        SpringApplication.run(PurchaseApplication.class, args);
     }
 }
