2ee2da991a26e4432bf84a9c5845815a61b62da2
Yurii Matora <yura.matora11@gmail.com>
2019-12-04 17:08:50 +0200

    feat: remove local properties and include two supplier services

---
+++ search-supplier/src/test/java/com/topparts/SearchSupplierServiceTest.java
@@ -0,0 +1,18 @@
+package com.topparts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple SearchSupplierService.
+ */
+public class SearchSupplierServiceTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
