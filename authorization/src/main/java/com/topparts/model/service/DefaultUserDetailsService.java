d8c063f97feace5cb1eea9cfd657a759f3a77f03
Yurii Matora <yura.matora11@gmail.com>
2019-12-08 01:37:51 +0200

    feat: implement security using JWT

---
+++ authorization/src/main/java/com/topparts/model/service/DefaultUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.topparts.model.service;
+
+import com.topparts.model.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public DefaultUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Can not find user: " + username));
+    }
+}
